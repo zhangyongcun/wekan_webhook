1aa6877f98424f48aeb8a0711e43389af1a6a69f
zhangyongcun <zhang.yongcun@ny.renderg.com>
2020-03-26 16:31:05 +0800

    update str format

--- main.py
+++ main.py
@@ -12,17 +12,16 @@ def hello():
     user = request.json.get('user')
     card = request.json.get('card')
     text = request.json.get('text')
+    msg = ''
     if description == 'act-createCard':  # 添加卡片
         msg = '{} 添加卡片 {}'.format(user, card)
     if description == 'act-joinMember':  # 添加成员:
-        msg = '有新的任务卡片: {}'.format(text)
+        msg = '{} 添加成员: {}'.format(user, text)
     if description == 'act-moveCard':  # 移动卡片
-        if '完成' in text:
-            msg = '完成: {}'.format(card)
-        if '进行中' in text:
-            msg = '进行中: {}'.format(card)
-    cmd = './pushWechatMsg --msg "{}"'.format(msg)
-    os.system(cmd)
+        msg = text.replace('Default', '').replace(' 到看板', '').replace(' 泳道', '').replace('Dev', '').replace('列表', '')
+    if msg:
+        cmd = './pushWechatMsg --msg "{}"'.format(msg)
+        os.system(cmd)
     return jsonify({
         'code': 200,
         'msg': 'success'
